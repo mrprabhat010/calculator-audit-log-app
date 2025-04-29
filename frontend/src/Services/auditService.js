500df29941f0b68a72e8bb3bf941038cda5675ac
mrprabhat010 <mrprabhat010@gmail.com>
2025-04-29 22:30:36 +0530

    removed console log

--- frontend/src/Services/auditService.js
+++ frontend/src/Services/auditService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =  import.meta.env.VITE_API_BASE_URL;
-console.log(API_BASE_URL)
+
 const auditService = {
   logAction: async (actionData) => {
     try {
